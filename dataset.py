fbeb86beab5b3d05fe21c49dbb3cc1dd1e4463e0
Jack-lx-jiang <linxijiang16@outlook.com>
2018-09-06 16:03:18 +0800

    fixes some bugs in generator() in dataset.py

--- dataset.py
+++ dataset.py
@@ -9,6 +9,7 @@ from keras.models import Model
 import pickle as p
 import os
 
+
 class Dictionary(object):
     def __init__(self, ans2idx=None, idx2ans=None):
         if ans2idx is None:
@@ -33,7 +34,7 @@ class Dictionary(object):
                 tokens.append(self.add_ans(a))
         else:
             for a in ans:
-                tokens.append(self.ans2idx(a))
+                tokens.append(self.ans2idx[a])
         return tokens
 
     def dump_to_file(self, path):
@@ -56,11 +57,13 @@ class Dictionary(object):
     def __len__(self):
         return len(self.idx2ans)
 
+
 class Dataset():
     # phases = ['train', 'test', 'val']
     phases = ['train', 'test']
     base_dir = 'dataset'
     vocabulary_size = 10000
+    feature_dir = base_dir + '/feature'
 
     def __init__(self):
         self.dict = Dictionary()
@@ -76,60 +79,60 @@ class Dataset():
 
     # split text data into three parts: video_id, questions and answers.
     def preprocess_text(self, phase):
-        assert(phase in self.phases)
+        assert (phase in self.phases)
 
         vid = []
         questions = []
         answers = []
-        fs = open(self.base_dir+'/'+phase+'.txt')
+        fs = open(self.base_dir + '/' + phase + '.txt')
         for line in fs.readlines():
             parts = line.split(',')
             vid.append(parts[0])
-            qs = 1
             for i in range(5):
-                questions.append(parts[i])
-                answers.extend(parts[i:i+3])
+                questions.append(parts[i * 4 + 1])
+                answers.extend(parts[i * 4 + 2:i * 4 + 5])
         return vid, questions, answers
 
     # the generator function for model's input
     def generator(self, batch_size, phase):
-        assert(phase in self.phases)
+        assert (phase in self.phases)
 
         vid, questions, answers = self.preprocess_text(phase)
-        questions = self.tokenizer.text_to_sequences(questions)
+        questions = self.tokenizer.texts_to_sequences(questions)
         answers = self.dict.tokenize(answers)
-        answer_size = max(answers)
-        one_hot_answers = [to_categorical(answers[i],answer_size)+\
-                           to_categorical(answers[i+1],answer_size)+\
-                           to_categorical(answers[i+2],answer_size)\
-                            for i in range(0,len(answers),3)]
+        self.answer_size = max(answers) + 1
+        one_hot_answers = [to_categorical(answers[i], self.answer_size) + \
+                           to_categorical(answers[i + 1], self.answer_size) + \
+                           to_categorical(answers[i + 2], self.answer_size) \
+                           for i in range(0, len(answers), 3)]
 
-        inds = [i for i in range(len(vid)*5)]
-        assert(len(inds) == len(questions))
-        assert(len(one_hot_answers) == len(questions))
+        inds = [i for i in range(len(vid) * 5)]
+        assert (len(inds) == len(questions))
+        assert (len(one_hot_answers) == len(questions))
         while True:
             if phase == 'train':
                 random.shuffle(inds)
             count = 0
-            while(count<len(inds)):
+            while (count < len(inds)):
                 X_video = np.zeros((batch_size, self.max_video_len, self.frame_size))
-                X_question = np.zeros((batch_size, self.max_question_len), dytpe=np.int32)
-                Y = np.zeros((batch_size, answer_size), dytpe=np.int32)
+                X_question = np.zeros((batch_size, self.max_question_len), dtype=np.int32)
+                Y = np.zeros((batch_size, self.answer_size), dtype=np.int32)
                 i = 0
                 j = 0
                 while i < batch_size:
                     try:
                         # load feature map of each frame
-                        cur_video = np.load(self.feature_dir+'/'+vid[(count+j)%len(inds)//5]+'_resnet.npy')
+                        cur_video = np.load(self.feature_dir + '/' + vid[(count + j) % len(inds) // 5] + '_resnet.npy')
                         X_video[i, :cur_video.shape[0]] = cur_video[:self.max_video_len]
-                        q = questions[inds[(count+j)%len(inds)]]
+                        q = questions[inds[(count + j) % len(inds)]]
                         X_question[i, :len(q)] = q
+                        Y[i, :] = one_hot_answers[(count + j) % len(inds)]
                         i += 1
-                        Y[i,:] = one_hot_answers[(count+j)%len(inds)]
-                    except:
-                        print('generator error')
+                    except Exception as e:
+                        print('generator error:')
+                        print(str(e))
                 j += 1
-
+            Y[Y > 1] = 1
             yield [X_video, X_question], Y
             count += j
 
@@ -140,14 +143,16 @@ class Dataset():
         res_model = ResNet50(weights='imagenet')
         model = Model(inputs=res_model.input, outputs=res_model.get_layer('avg_pool').output)
 
-        self.feature_dir = self.base_dir+'/feature'
-
         for phase in self.phases:
-            vid_dir = self.base_dir+'/'+phase
+            vid_dir = self.base_dir + '/' + phase
             for i, v in enumerate(os.listdir(vid_dir)):
-                video = imageio.get_reader(vid_dir+'/'+str(v), 'ffmpeg')
-                print(vid_dir+'/'+str(v), i)
-                vid_descriptors = np.zeros((999*batch_size, 2048))
+                feature_file = self.feature_dir + '/' + v.split('.')[0] + \
+                               '_resnet.npy';
+                if os.path.exists(feature_file):
+                    continue
+                video = imageio.get_reader(vid_dir + '/' + str(v), 'ffmpeg')
+                print(vid_dir + '/' + str(v), i)
+                vid_descriptors = np.zeros((999 * batch_size, 2048))
                 frame_count = 0
                 frame_ind = 0
                 stop = False
@@ -160,7 +165,7 @@ class Dataset():
                             stop = True
                             break
                         else:
-                            frame = transform.resize(frame,(224,224))
+                            frame = transform.resize(frame, (224, 224))
                             batch[t] = frame
                             frame_count += 1
 
@@ -168,10 +173,9 @@ class Dataset():
                     batch[:, :, :, 0] -= 103.939
                     batch[:, :, :, 1] -= 116.779
                     batch[:, :, :, 2] -= 123.68
-                    vid_descriptors[b*batch_size:(b+1)*batch_size] = model.predict_on_batch(batch).reshape(batch.shape[0],-1)
+                    vid_descriptors[b * batch_size:(b + 1) * batch_size] = model.predict_on_batch(batch).reshape(
+                        batch.shape[0], -1)
                     if stop:
                         video.close()
                         break
-                np.save(self.feature_dir+'/'+v.split('.')[0]+\
-                        '_resnet.npy', vid_descriptors[:frame_count])
-
+                np.save(feature_file, vid_descriptors[:frame_count])
--- generate_frame_feature.py
+++ generate_frame_feature.py
@@ -1,3 +1,10 @@
 from dataset import Dataset
 d = Dataset()
 d.compute_frame_feature()
+
+# for x, y in d.generator(128, 'train'):
+#     assert(x[1].shape==(128,d.max_question_len))
+#     assert(x[0].shape==(128,d.max_video_len,d.frame_size))
+#     assert(y.shape==(128,d.answer_size))
+#     if sum(y)> 128*3 or sum(y)<128:
+#         print('error!!!')
