9a407451e4e3d16321cad569eb2181dab6d0ef0f
KaitoHH <hh.kaito@gmail.com>
2018-09-12 17:06:40 +0800

    fix data path

--- test/test_generate_feature.py
+++ test/test_generate_feature.py
@@ -4,6 +4,6 @@ from dataset import Dataset
 
 class FeatureGenerationTest(unittest.TestCase):
     def test(self):
-        dataset = Dataset(phase='train', base_dir='sample_data')
+        dataset = Dataset(phase='train', base_dir='test/sample_data')
         dataset.phases = ['train']
         dataset.compute_frame_feature()
