92b234808a75074b9c556c719767bf95d9a5005d
KaitoHH <hh.kaito@gmail.com>
2018-09-12 23:17:23 +0800

    suppress deprecation warning, add shape checking test

--- generate_frame_feature.py
+++ generate_frame_feature.py
@@ -1,14 +1,3 @@
 from dataset import Dataset
 d = Dataset()
-# d.compute_frame_feature()
-
-count = 0
-for x, y in d.generator(128, 'train'):
-    assert (x[1].shape == (128, d.max_question_len))
-    assert (x[0].shape == (128, d.max_video_len, d.frame_size))
-    assert (y.shape == (128, d.answer_size))
-    # print(count)
-    count += 1
-    # print(y.sum())
-    if y.sum() > 128 * 3 or y.sum() < 128:
-        print('error!!!')
+d.compute_frame_feature()
--- test/test_generate_feature.py
+++ test/test_generate_feature.py
@@ -1,9 +1,20 @@
+import itertools
+import random
 import unittest
+
 from dataset import Dataset
 
 
 class FeatureGenerationTest(unittest.TestCase):
-    def test(self):
-        dataset = Dataset(phase='train', base_dir='test/sample_data')
-        dataset.phases = ['train']
-        dataset.compute_frame_feature()
+    def test_generate_train_dataset_feature(self):
+        data = Dataset(phase='train', base_dir='test/sample_data')
+        data.phases = ['train']
+        data.compute_frame_feature()
+        batch_size = 128
+        for x, y in itertools.islice(data.generator(batch_size, 'train'), random.randint(5, 20)):
+            self.assertEqual(x[1].shape, (batch_size, data.max_question_len))
+            self.assertEqual(x[0].shape, (batch_size, data.max_video_len, data.frame_size))
+            self.assertEqual(y.shape, (batch_size, data.answer_size))
+            total = y.sum()
+            self.assertLessEqual(total, batch_size * 3)
+            self.assertGreaterEqual(total, batch_size)
