2261426f5589c08beeccff962bc8c7a198448504
KaitoHH <hh.kaito@gmail.com>
2018-09-18 23:16:12 +0800

    add submit

--- test/test_train_model.py
+++ test/test_train_model.py
@@ -1,5 +1,19 @@
 import unittest
 
+from keras.optimizers import Adadelta
+
+from dataset import Dataset
+from util.loss import focal_loss
+from util.metrics import multians_accuracy
+
 
 class TrainModelTest(unittest.TestCase):
-    pass
+    def test_base_model(self):
+        data = Dataset(phase='train', base_dir='test/sample_data')
+        views = __import__('model')
+        cur_model = getattr(views, 'base_model')
+        model = cur_model(data.vocabulary_size, data.max_question_len, data.max_video_len, data.frame_size,
+                          data.answer_size)
+        model.compile(optimizer=Adadelta(), loss=[focal_loss(alpha=.25, gamma=2)], metrics=[multians_accuracy])
+        model.fit_generator(data.generator(128, 'train'), 10)
+        result = model.predict_generator(data.generator(128, 'train'), 10)
