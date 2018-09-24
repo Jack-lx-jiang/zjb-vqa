848d508e2491be1cd5037c4671c3e588b6c56d60
KaitoHH <hh.kaito@gmail.com>
2018-09-24 15:04:39 +0800

    bug fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from dataset import Dataset
-from subprocess import call
 import datetime
+from subprocess import call
+
+from dataset import Dataset
 
 # This needs to be changed
 data_dir = '../data/DatasetA'
@@ -8,19 +9,25 @@ data_dir = '../data/DatasetA'
 
 def main():
     # generate feature (this should run once)
+    print('Generating video feature...')
     d = Dataset(base_dir=data_dir)
     d.compute_frame_feature()
 
     exp_name = 'experiments/encode_decode_model/'
 
     # train model
-    call(['python3', 'vqa.py', '--data_dir', data_dir, '--exp_name', exp_name, 'encode_decode_model', 'train'])
+    print('Now training model...')
+    call(
+        ['python', 'vqa.py', '--data_dir', data_dir, '--exp_name', exp_name, 'encode_decode_model', 'train', '--epoch',
+         '20'])
 
     # test model
-    call(['python3', 'vqa.py', '--data_dir', data_dir, '--exp_name', exp_name, 'test'])
+    print('Now testing model...')
+    call(['python', 'vqa.py', '--data_dir', data_dir, '--exp_name', exp_name, 'encode_decode_model', 'test'])
 
     # copy submit.txt
     call(['cp', 'submit.txt', "../submit/submit_" + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + ".txt"])
+    print('Finished.')
 
 
 if __name__ == '__main__':
