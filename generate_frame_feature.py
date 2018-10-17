019c55f2b2c8bd165740842a6dc6ba4ddb053931
KaitoHH <hh.kaito@gmail.com>
2018-10-17 15:17:41 +0800

    remove unneeded file

--- generate_frame_feature.py
+++
@@ -1,4 +0,0 @@
-from dataset import Dataset
-
-d = Dataset(base_dir='dataset2')
-d.compute_frame_feature()
--- text_analysis.py
+++
@@ -1,8 +0,0 @@
-path = '/Users/jack/Downloads/train.txt'
-
-fs = open(path, 'r+')
-s = set()
-for line in fs.readlines():
-    s.update(line.split(',')[1:])
-print(len(s))
-fs.close()
